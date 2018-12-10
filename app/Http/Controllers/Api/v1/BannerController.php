6dd2a0056d36373a3c84e90f2657c93959458e97
WuHuangCheng <516100080@qq.com>
2018-12-10 15:15:20 +0800

    修改文件上传代码

--- app/Http/Controllers/Api/v1/BannerController.php
+++ app/Http/Controllers/Api/v1/BannerController.php
@@ -12,6 +12,7 @@ use App\Exceptions\AppException;
 use App\Http\Controllers\Controller;
 use App\Models\Banner;
 use App\Services\BannerService;
+use App\Services\FileService;
 use App\Utils\ImgUtil;
 use App\Utils\ResponseUtil;
 use Illuminate\Support\Facades\Storage;
@@ -123,39 +124,13 @@ class BannerController extends Controller
      * @param Request $request
      * @return string
      * @throws AppException
+     * banner上传
      */
     public function upload(Request $request)
     {
-        if ($request->isMethod('post')) {
-            $file = $request->file('file');
-            // 文件是否上传成功
-            if ($file->isValid()) {
-                // 获取文件相关信息
-                $ext = $file->getClientOriginalExtension();     // 扩展名
-                $realPath = $file->getRealPath();   //临时文件的绝对路径
-                // 上传文件
-                $filename = $this->filename(uniqid()) . '.' . $ext;
-                $bool = Storage::disk('uploads')->put($filename, file_get_contents($realPath));
-                if($bool)
-                {
-                    $imgUtil = new ImgUtil();
-                    $data['url'] = $imgUtil->ossUpload($filename,'banner');
-                    return ResponseUtil::toJson($data);
-                }
-                throw new AppException('上传失败');
-            }
-            throw new AppException('文件上传出错');
-        }
+        $data = (new FileService())->upload($request,'banner');
+        return ResponseUtil::toJson($data);
     }
 
-    /**
-     * @param $data
-     * @return string
-     * 获得文件名
-     */
-    private function filename($data)
-    {
-        $filename = md5(time() . $data);
-        return $filename;
-    }
+
 }
